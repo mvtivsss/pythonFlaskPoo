16c06c6dc3118b69d60492674951067b5fff9b6e
Matias Vergara <mvarancibia68@gmail.com>
2021-11-04 01:04:49 -0300

    eliminacion de archivos que contienen datos en duro.

--- clients.py
+++
@@ -1,5 +0,0 @@
-clientes = [
-    {'id': 1, 'nombre': 'Marcos Fuentes','cargo':'DEV'},
-    {'id': 2, 'nombre': 'Miguel Rojas','cargo':'Jefe de Proyecto'},
-    {'id': 3, 'nombre': 'Matias Vergara','cargo':'DEV'}
-]
--- regions.py
+++
@@ -1,6 +0,0 @@
-regions = [
-    {'id': 1, 'nombre': 'Region Metropolitana'},
-    {'id': 2, 'nombre': 'Puerto Varas'},
-    {'id': 3, 'nombre': 'La Serena'},
-    {'id': 4, 'nombre': 'Arica'}
-]
