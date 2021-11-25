4b20fa4f6a6b97e64802971fd74936a9dd388453
Matias Vergara <mvarancibia68@gmail.com>
2021-11-25 00:40:51 -0300

    fix errors SP PL/SQL

--- BD/spGets.sql
+++ BD/spGets.sql
@@ -34,6 +34,68 @@ CREATE OR REPLACE PROCEDURE TURISMO.spGetComuna
 END spGetComuna;
 /
 
+CREATE OR REPLACE PROCEDURE TURISMO.SPGETDEPARTMENTBYDISPONIBILITY(DISPONIBILITY  IN  TURISMO.DEPARTAMENTO.ESTADO_DEPTO % TYPE,
+                                                                   CUR OUT SYS_REFCURSOR)
+  AS
+  BEGIN
+    OPEN CUR FOR
+    SELECT "d".ID_DEPTO,
+           "d".NOM_DEPTO,
+           "d".DIRECCION_DEPTO,
+           "d".CANT_HABITACIONES,
+           "d".CANT_ESTACIONAMIENTOS,
+           "d".CANT_BANOS,
+           "d".INTERNET,
+           "d".CABLE,
+           "d".CALEFACCION,
+           "d".AMOBLADO,
+           "d".PRECIO_DEPTO,
+           "d".ESTADO_DEPTO,
+           "r".NOMBRE_REGION || ', ' || "c1".NOMBRE_CIUDAD || ', ' || "c".NOMBRE_COMUNA AS UBICACION
+      FROM DEPARTAMENTO "d"
+        JOIN COMUNA "c"
+          ON "d".COMUNA_ID_COMUNA = "c".ID_COMUNA
+        JOIN CIUDAD "c1"
+          ON "c".CIUDAD_ID_CIUDAD = "c1".ID_CIUDAD
+        JOIN REGION "r"
+          ON "c1".REGION_ID_REGION = "r".ID_REGION
+      WHERE "d".ESTADO_DEPTO = DISPONIBILITY;
+  EXCEPTION
+    WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM());
+  END;
+/
+
+CREATE OR REPLACE PROCEDURE TURISMO.SPGETDEPARTMENTBYID(ID  IN  TURISMO.DEPARTAMENTO.ID_DEPTO % TYPE,
+                                                        CUR OUT SYS_REFCURSOR)
+  AS
+  BEGIN
+    OPEN CUR FOR
+    SELECT "d".ID_DEPTO,
+           "d".NOM_DEPTO,
+           "d".DIRECCION_DEPTO,
+           "d".CANT_HABITACIONES,
+           "d".CANT_ESTACIONAMIENTOS,
+           "d".CANT_BANOS,
+           "d".INTERNET,
+           "d".CABLE,
+           "d".CALEFACCION,
+           "d".AMOBLADO,
+           "d".PRECIO_DEPTO,
+           "d".ESTADO_DEPTO,
+           "r".NOMBRE_REGION || ', ' || "c1".NOMBRE_CIUDAD || ', ' || "c".NOMBRE_COMUNA AS UBICACION
+      FROM DEPARTAMENTO "d"
+        JOIN COMUNA "c"
+          ON "d".COMUNA_ID_COMUNA = "c".ID_COMUNA
+        JOIN CIUDAD "c1"
+          ON "c".CIUDAD_ID_CIUDAD = "c1".ID_CIUDAD
+        JOIN REGION "r"
+          ON "c1".REGION_ID_REGION = "r".ID_REGION
+      WHERE "d".ID_DEPTO = ID;
+  EXCEPTION
+    WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM());
+  END;
+/
+
 CREATE OR REPLACE PROCEDURE TURISMO.SPGETDEPARTMENTS(PDEPARTMENTS OUT SYS_REFCURSOR)
   AS
   BEGIN
@@ -87,6 +149,27 @@ CREATE OR REPLACE PROCEDURE TURISMO.SPGETINVENTORYDEPARTMENT(PINVENTORYDEPARTMEN
   END;
 /
 
+CREATE OR REPLACE PROCEDURE TURISMO.SPGETMAINTAINDEPARTMENTBYID(id IN  NUMBER,
+                                                                CUR OUT SYS_REFCURSOR)
+  AS
+  BEGIN
+    OPEN CUR FOR
+    SELECT "m".ID_MANTENCION,
+           TO_CHAR("m".FECHA_INICIO, 'DD/MM/YYYY'),
+           TO_CHAR("m".FECHA_TERMINO, 'DD/MM/YYYY'),
+           "u".ID_USU,
+           "u".NOM_USU || '-' || "u".APP_USU || '-' || "u".APM_USU AS NAMEUSER,
+           "m".DEPARTAMENTO_ID_DEPTO
+      FROM MANTENCION "m" 
+        JOIN USUARIO "u"
+          ON "m".ID_FUNCIONARIO = "u".ID_USU
+        JOIN DEPARTAMENTO "d"
+          ON "m".DEPARTAMENTO_ID_DEPTO = "d".ID_DEPTO
+      WHERE "m".DEPARTAMENTO_ID_DEPTO = id;
+      EXCEPTION WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM());
+  END;
+/
+
 CREATE OR REPLACE PROCEDURE TURISMO.SPGETMAINTAINSDEPARTMENT(PMAINTAINS OUT SYS_REFCURSOR)
   AS
   BEGIN
@@ -105,6 +188,23 @@ CREATE OR REPLACE PROCEDURE TURISMO.SPGETMAINTAINSDEPARTMENT(PMAINTAINS OUT SYS_
   END;
 /
 
+CREATE OR REPLACE PROCEDURE TURISMO.SPGETMULTA(IDRESERVA IN  TURISMO.RESERVA_ACTA.ID % TYPE,
+                                               CUR       OUT SYS_REFCURSOR)
+  AS
+  BEGIN
+    OPEN CUR FOR
+    SELECT "ra".ID,
+           "ra".CANTIDAD_ACTA,
+           "ra".SUBTOTAL_ACTA,
+           "ra".ACTA_ID_ACTA,
+           "ra".RESERVA_ID_RESERVA
+      FROM RESERVA_ACTA "ra"
+      WHERE "ra".RESERVA_ID_RESERVA = IDRESERVA;
+  EXCEPTION
+    WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM());
+  END;
+/
+
 CREATE OR REPLACE PROCEDURE TURISMO.spGetRegion
   (pRegion out sys_refcursor)
  AS
@@ -115,169 +215,6 @@ CREATE OR REPLACE PROCEDURE TURISMO.spGetRegion
     WHEN OTHERS THEN
         dbms_output.put_line(sqlerrm);
 END spGetRegion;
-/
-
-CREATE OR REPLACE PROCEDURE TURISMO.spGetServExtra
-  (pServicio out sys_refcursor)
- AS
- BEGIN
-  OPEN pServicio FOR
-      SELECT ID_SERV, DESC_SERV, VALOR_SERV FROM SERVICIO_EXTRA;
-    EXCEPTION
-    WHEN OTHERS THEN
-        dbms_output.put_line(sqlerrm);
-END spGetServExtra;
-/
-
-CREATE OR REPLACE PROCEDURE TURISMO.SPGETUSERS(PUSER OUT SYS_REFCURSOR)
-  AS
-  BEGIN
-    OPEN PUSER FOR
-    SELECT "u".ID_USU,
-           "u".RUT_USU,
-           "u".NOM_USU,
-           "u".APP_USU,
-           "u".APM_USU,
-           TO_CHAR("u".FNACIMIENTO_USU,'DD/MM/YYYY'),
-           "u".EMAIL_USU,
-           "u".CELULAR_USU,
-           "u".CONTRASENA_USU,
-           "c".ID_COMUNA,
-           "c".NOMBRE_COMUNA,
-           "tu".ID_TIPO,
-           "tu".NOMBRE_TIPO
-      FROM USUARIO "u"
-        JOIN COMUNA "c"
-          ON "u".COMUNA_ID_COMUNA = "c".ID_COMUNA
-        JOIN TIPO_USUARIO "tu"
-          ON "u".TIPO_USUARIO_ID_TIPO = "tu".ID_TIPO;
-  EXCEPTION
-    WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM);
-  END;
-/
-
-CREATE OR REPLACE PROCEDURE TURISMO.spGetTypeUser (pTypeUsers OUT SYS_REFCURSOR)
- AS
- BEGIN
- OPEN pTypeUsers FOR                
-  SELECT "tu".ID_TIPO, "tu".NOMBRE_TIPO FROM TIPO_USUARIO "tu";
-  EXCEPTION WHEN 
-  OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM());
-END;
-/
-
-CREATE OR REPLACE PROCEDURE TURISMO.SPGETDEPARTMENTBYID(ID  IN  TURISMO.DEPARTAMENTO.ID_DEPTO % TYPE,
-                                                        CUR OUT SYS_REFCURSOR)
-  AS
-  BEGIN
-    OPEN CUR FOR
-    SELECT "d".ID_DEPTO,
-           "d".NOM_DEPTO,
-           "d".DIRECCION_DEPTO,
-           "d".CANT_HABITACIONES,
-           "d".CANT_ESTACIONAMIENTOS,
-           "d".CANT_BANOS,
-           "d".INTERNET,
-           "d".CABLE,
-           "d".CALEFACCION,
-           "d".AMOBLADO,
-           "d".PRECIO_DEPTO,
-           "d".ESTADO_DEPTO,
-           "r".NOMBRE_REGION || ', ' || "c1".NOMBRE_CIUDAD || ', ' || "c".NOMBRE_COMUNA AS UBICACION
-      FROM DEPARTAMENTO "d"
-        JOIN COMUNA "c"
-          ON "d".COMUNA_ID_COMUNA = "c".ID_COMUNA
-        JOIN CIUDAD "c1"
-          ON "c".CIUDAD_ID_CIUDAD = "c1".ID_CIUDAD
-        JOIN REGION "r"
-          ON "c1".REGION_ID_REGION = "r".ID_REGION
-      WHERE "d".ID_DEPTO = ID;
-  EXCEPTION
-    WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM());
-  END;
-/
-
-CREATE OR REPLACE PROCEDURE TURISMO.SPGETMAINTAINDEPARTMENTBYID(COD IN  NUMBER,
-                                                                CUR OUT SYS_REFCURSOR)
-  AS
-  BEGIN
-    OPEN CUR FOR
-    SELECT "m".ID_MANTENCION,
-           TO_CHAR("m".FECHA_INICIO, 'DD/MM/YYYY'),
-           TO_CHAR("m".FECHA_TERMINO, 'DD/MM/YYYY'),
-           "u".ID_USU,
-           "u".NOM_USU || '-' || "u".APP_USU || '-' || "u".APM_USU AS NAMEUSER,
-           "m".DEPARTAMENTO_ID_DEPTO
-      FROM MANTENCION "m" 
-        JOIN USUARIO "u"
-          ON "m".ID_FUNCIONARIO = "u".ID_USU
-        JOIN DEPARTAMENTO "d"
-          ON "m".DEPARTAMENTO_ID_DEPTO = "d".ID_DEPTO
-      WHERE "m".DEPARTAMENTO_ID_DEPTO = COD;
-
-  END;
-/
-
-CREATE OR REPLACE PROCEDURE TURISMO.SPGETDEPARTMENTBYID(ID  IN  TURISMO.DEPARTAMENTO.ID_DEPTO % TYPE,
-                                                        CUR OUT SYS_REFCURSOR)
-  AS
-  BEGIN
-    OPEN CUR FOR
-    SELECT "d".ID_DEPTO,
-           "d".NOM_DEPTO,
-           "d".DIRECCION_DEPTO,
-           "d".CANT_HABITACIONES,
-           "d".CANT_ESTACIONAMIENTOS,
-           "d".CANT_BANOS,
-           "d".INTERNET,
-           "d".CABLE,
-           "d".CALEFACCION,
-           "d".AMOBLADO,
-           "d".PRECIO_DEPTO,
-           "d".ESTADO_DEPTO,
-           "r".NOMBRE_REGION || ', ' || "c1".NOMBRE_CIUDAD || ', ' || "c".NOMBRE_COMUNA AS UBICACION
-      FROM DEPARTAMENTO "d"
-        JOIN COMUNA "c"
-          ON "d".COMUNA_ID_COMUNA = "c".ID_COMUNA
-        JOIN CIUDAD "c1"
-          ON "c".CIUDAD_ID_CIUDAD = "c1".ID_CIUDAD
-        JOIN REGION "r"
-          ON "c1".REGION_ID_REGION = "r".ID_REGION
-      WHERE "d".ID_DEPTO = ID;
-  EXCEPTION
-    WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM());
-  END;
-/
-
-CREATE OR REPLACE PROCEDURE TURISMO.SPGETDEPARTMENTBYDISPONIBILITY(DISPONIBILITY  IN  TURISMO.DEPARTAMENTO.ESTADO_DEPTO % TYPE,
-                                                                   CUR OUT SYS_REFCURSOR)
-  AS
-  BEGIN
-    OPEN CUR FOR
-    SELECT "d".ID_DEPTO,
-           "d".NOM_DEPTO,
-           "d".DIRECCION_DEPTO,
-           "d".CANT_HABITACIONES,
-           "d".CANT_ESTACIONAMIENTOS,
-           "d".CANT_BANOS,
-           "d".INTERNET,
-           "d".CABLE,
-           "d".CALEFACCION,
-           "d".AMOBLADO,
-           "d".PRECIO_DEPTO,
-           "d".ESTADO_DEPTO,
-           "r".NOMBRE_REGION || ', ' || "c1".NOMBRE_CIUDAD || ', ' || "c".NOMBRE_COMUNA AS UBICACION
-      FROM DEPARTAMENTO "d"
-        JOIN COMUNA "c"
-          ON "d".COMUNA_ID_COMUNA = "c".ID_COMUNA
-        JOIN CIUDAD "c1"
-          ON "c".CIUDAD_ID_CIUDAD = "c1".ID_CIUDAD
-        JOIN REGION "r"
-          ON "c1".REGION_ID_REGION = "r".ID_REGION
-      WHERE "d".ESTADO_DEPTO = DISPONIBILITY;
-  EXCEPTION
-    WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM());
-  END;
 /
 
 CREATE OR REPLACE PROCEDURE TURISMO.SPGETRESERVA(CUR OUT SYS_REFCURSOR)
@@ -319,23 +256,6 @@ CREATE OR REPLACE PROCEDURE TURISMO.SPGETRESERVA(CUR OUT SYS_REFCURSOR)
   END;
 /
 
-CREATE OR REPLACE PROCEDURE TURISMO.SPGETMULTA(IDRESERVA IN  TURISMO.RESERVA_ACTA.ID % TYPE,
-                                               CUR       OUT SYS_REFCURSOR)
-  AS
-  BEGIN
-    OPEN CUR FOR
-    SELECT "ra".ID,
-           "ra".CANTIDAD_ACTA,
-           "ra".SUBTOTAL_ACTA,
-           "ra".ACTA_ID_ACTA,
-           "ra".RESERVA_ID_RESERVA
-      FROM RESERVA_ACTA "ra"
-      WHERE "ra".RESERVA_ID_RESERVA = IDRESERVA;
-  EXCEPTION
-    WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM());
-  END;
-/
-
 CREATE OR REPLACE PROCEDURE TURISMO.SPGETRESERVASERVEX(IDRESERVA IN  TURISMO.RESERVA_ACTA.ID % TYPE,
                                                        CUR       OUT SYS_REFCURSOR)
   AS
@@ -350,5 +270,54 @@ CREATE OR REPLACE PROCEDURE TURISMO.SPGETRESERVASERVEX(IDRESERVA IN  TURISMO.RES
       WHERE "rs".RESERVA_ID_RESERVA = IDRESERVA;
   EXCEPTION
     WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM());
+  END;
+/
+
+CREATE OR REPLACE PROCEDURE TURISMO.spGetServExtra
+  (pServicio out sys_refcursor)
+ AS
+ BEGIN
+  OPEN pServicio FOR
+      SELECT ID_SERV, DESC_SERV, VALOR_SERV FROM SERVICIO_EXTRA;
+    EXCEPTION
+    WHEN OTHERS THEN
+        dbms_output.put_line(sqlerrm);
+END spGetServExtra;
+/
+
+CREATE OR REPLACE PROCEDURE TURISMO.spGetTypeUser (pTypeUsers OUT SYS_REFCURSOR)
+ AS
+ BEGIN
+ OPEN pTypeUsers FOR                
+  SELECT "tu".ID_TIPO, "tu".NOMBRE_TIPO FROM TIPO_USUARIO "tu";
+  EXCEPTION WHEN 
+  OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM());
+END;
+/
+
+CREATE OR REPLACE PROCEDURE TURISMO.SPGETUSERS(PUSER OUT SYS_REFCURSOR)
+  AS
+  BEGIN
+    OPEN PUSER FOR
+    SELECT "u".ID_USU,
+           "u".RUT_USU,
+           "u".NOM_USU,
+           "u".APP_USU,
+           "u".APM_USU,
+           TO_CHAR("u".FNACIMIENTO_USU,'DD/MM/YYYY'),
+           "u".EMAIL_USU,
+           "u".CELULAR_USU,
+           "u".CONTRASENA_USU,
+           "c".ID_COMUNA,
+           "c".NOMBRE_COMUNA,
+           "tu".ID_TIPO,
+           "tu".NOMBRE_TIPO
+      FROM USUARIO "u"
+        JOIN COMUNA "c"
+          ON "u".COMUNA_ID_COMUNA = "c".ID_COMUNA
+        JOIN TIPO_USUARIO "tu"
+          ON "u".TIPO_USUARIO_ID_TIPO = "tu".ID_TIPO;
+  EXCEPTION
+    WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM);
   END;
 /
